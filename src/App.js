771c28fcfd42fab8ed535f1a2131d7b37bfb76ea
Kerrn Reehal <kreehal@berkeley.edu>
2022-04-22 15:42:55 -0700

    added src

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav.js';
+import Home from './components/Home.js';
+import Tutor from './components/Tutor.js';
+import Resource from './components/Resource.js';
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Nav />
+          <Routes>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/tutors" component={Tutor} />
+            <Route exact path="/resources" component={Resource} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
